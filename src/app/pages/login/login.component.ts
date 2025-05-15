f471a3429d7e8580611e4781e7479fb9056eb5b5
nextor <nextor@gmail.com>
2025-05-15 00:12:31 -0700

    Fix error

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,14 +6,14 @@ interface CLogin {
 	password: string;
 }
 @Component({
-  selector: 'app-login',
-  imports: [CommonModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+	selector: 'app-login',
+	imports: [CommonModule],
+	templateUrl: './login.component.html',
+	styleUrl: './login.component.css'
 })
 export class LoginComponent extends BaseComponent
 {
-  is_loading: boolean = true;
+	is_loading: boolean = true;
 	doLogin(evt: Event)
 	{
 		evt.preventDefault();
@@ -21,26 +21,36 @@ export class LoginComponent extends BaseComponent
 
 		this.is_loading = true;
 
-		this.subs.sink = this.doLogin_starter(this.username, this.password)
-		.subscribe
-		({
-			next: (response) =>
-			{
-				this.is_loading = false;
-				if (this.return_url)
-				{
-					this.router.navigate(['/main']);
-				} else
-				{
-					this.router.navigate(['/list-requisition']);
-				}
-				//this.showSuccess('Sesion iniciada con exito');
+
+		fetch('https://uniformesprofesionales.integranet.xyz/api/login.php',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
 			},
-			error: (error) =>
+			body: JSON.stringify({
+				username: this.username,
+				password: this.password
+			})
+		})
+		.then((response) => response.json())
+		.then((response) =>
+        {
+			this.is_loading = false;
+			if (this.return_url)
 			{
-				//this.showError(error);
-				//this.is_loading = false;
+				this.router.navigate(['/main']);
 			}
-		});
-	}
+            else
+			{
+				this.router.navigate(['/list-requisition']);
+				//this.showSuccess('Sesion iniciada con exito');
+			}
+        })
+        .catch((error) =>
+        {
+			this.is_loading = false;
+			this.showError(error);
+        });
+    }
 }
