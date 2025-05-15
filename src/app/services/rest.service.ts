98cfe693e838d1bf8f6774ac74be44464c3d377d
nextor <nextor@gmail.com>
2025-05-15 01:16:22 -0700

    removing errors

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GetEmpty } from '../models/GetEmpty';
 import { Ecommerce } from '../models/RestModels';
 
-@Injectable({
+@Injectable
+({
 	providedIn: 'root',
 })
-export class RestService {
+export class RestService
+{
 	getLoginLogo() {}
 
 	public ecommerce: Ecommerce = GetEmpty.ecommerce();
 
-	constructor(private http: HttpClient) {}
-
-	get(url: string): Observable<any> {
-		return this.http.get(url);
+	constructor()
+	{
 	}
 }
