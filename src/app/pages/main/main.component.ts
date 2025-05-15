c46996e3741d2fa4f06daba3fb9531a988ad4eca
nextor <nextor@gmail.com>
2025-05-15 06:28:54 +0000

    feat: add rest service and include on main and login components

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-main',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 	item_info_list:any[] = [];
+	constructor(public rest: RestService) {}
 
 		ngOnInit(): void
 		{
