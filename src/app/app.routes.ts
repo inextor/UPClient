d26eb7c8cb0cd98a5d7d4398c5463817ca53ad37
nextor <nextor@gmail.com>
2025-05-17 21:32:51 +0000

    Added cart component to router and staged changes for data inspection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'main', component: MainComponent },
+    { path: 'cart', component: CartComponent },
  { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
