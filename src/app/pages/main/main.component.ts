8229a2636708ad918a8c237907080c2f1be83519
nextor <nextor@gmail.com>
2025-05-23 07:22:30 +0000

    Gemini it tried to fix the pagination error

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -20,12 +20,10 @@ export class MainComponent extends BaseComponent
 	ngOnInit(): void
 	{
 		this.route.queryParamMap.subscribe((params) =>
-		{
-			let page = 1;
-			let page_size = 10;
-
+		{			
 			if( params.has('page') )
 			{
+
 				this.current_page =	parseInt(params.get('page') as string);
 			}
 
@@ -54,4 +52,40 @@ export class MainComponent extends BaseComponent
 	{
 		this.rest.addToCart(item_id, 1);
 	}
+
+	goToPage(page: number) {
+		if (page >= 1 && page <= this.total_pages) {
+		  this.current_page = page;
+		  this.fetchProducts(); // Call the function to fetch products for the new page
+		}
+	  }
+
+	  previousPage() {
+		if (this.current_page > 1) {
+		  this.goToPage(this.current_page - 1);
+		}
+	  }
+
+	  nextPage() {
+		if (this.current_page < this.total_pages) {
+		  this.goToPage(this.current_page + 1);
+		}
+	  }
+
+	  fetchProducts() {
+		let page_size = 10;
+		let url = this.rest.base_url + '/item_info.php?limit=' + page_size + '&page=' + this.current_page;
+
+		fetch(url)
+		  .then((response: any) => {
+			return response.json();
+		  })
+		  .then((response) => {
+			this.total_pages = response.total / 100 + (response.total % 100 ? 1 : 0);
+			this.item_info_list = response.data;
+		  })
+		  .catch((error) => {
+			console.log(error);
+		  });
+	  }
 }
