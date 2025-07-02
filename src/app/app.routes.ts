2a6ec78ec22220f1872951bedc24763018c63bff
nextor <nextor@gmail.com>
2025-07-01 22:08:27 -0700

    Adding logout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'main', component: MainComponent },
     { path: 'cart', component: CartComponent },
+    { path: 'logout', component: LogoutComponent },
  { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
