862b036651d04677a1e83cba88d1d807efedabf5
nextor <nextor@gmail.com>
2025-09-16 12:49:34 -0700

    Cambios en mejoras calculo precio y manejo de los datos

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -29,10 +29,14 @@ export class CartComponent extends BaseComponent implements OnInit
 	{
 		return this.cart_items.reduce((total, item) =>
 		{
-			const quantity = item.quantity || 0;
-			const price = item.price || 0;
-			return total + (quantity * price);
-	}, 0);
+				console.log(item);
+			let price = item.item_info.prices.find((price:any)=>
+			{
+				return price.price_type_id == this.rest.ecommerce.price_type_id
+					&& price.price_list_id == this.rest.ecommerce.price_list_id;
+			});
+			return total + (item.qty * price.price);
+		}, 0);
 	}
 
 	ngOnInit(): void { // Implement ngOnInit
@@ -40,7 +44,7 @@ export class CartComponent extends BaseComponent implements OnInit
 		this.route.paramMap.subscribe((params) =>
 		{
 			this.sync_uuid = this.rest.getUUID();
-			this.fetchCartItems();
+			this.fetchCartItems2();
 		});
 	}
 
@@ -68,12 +72,12 @@ export class CartComponent extends BaseComponent implements OnInit
 
 			this.cart_items = response.data.map((item_info:any) =>
 			{
+				console.log(item_info);
+				item_info.price = item_info.prices.find((price:any)=>true);
 				let cart_item = cart_items.find((item) => item.item_id === item_info.item.id);
 				let image_url = item_info.item.image_id ? this.rest.base_url + '/image.php?id=' + item_info.item.image_id : null;
 				return {...cart_item, ...item_info, image_url};
 			});
-
-			this.updateTotal();
 
 			const ecommerce_item_params = this.rest.getUrlParams({ 'item_id,': item_ids, ecommerce_id: this.rest.ecommerce.id });
 			this.rest.getEcommerceItems(ecommerce_item_params).then((ecommerce_items_response) => {
@@ -88,13 +92,63 @@ export class CartComponent extends BaseComponent implements OnInit
 		})
 	}
 
+	public fetchCartItems2()
+	{
+		let cart_items = this.rest.getCartItems();
+
+		if (cart_items.length === 0) {
+			this.cart_items = [];
+			return;
+		}
+
+		let item_ids = cart_items.map(item => item.item_id).join(',');
+
+		let params = this.rest.getUrlParams({ 'id,': item_ids })
+
+		Promise.all([ cart_items, this.rest.getItems(params) ])
+		.then(([ cart_items, response ]) =>
+		{
+			console.log(cart_items);
+			let item_info_list:any[] = response.data.map((item_info:any) =>
+			{
+				console.log(item_info);
+				item_info.price = item_info.prices.find((price:any)=>true);
+				let cart_item = cart_items.find((item) => item.item_id === item_info.item.id);
+				console.log('c+item',cart_item, item_info);
+				let image_url = item_info.item.image_id ? this.rest.base_url + '/image.php?id=' + item_info.item.image_id : null;
+				return {...cart_item, ...item_info, image_url};
+			});
+			console.log(item_info_list);
+
+			const ecommerce_item_params = this.rest.getUrlParams({ 'item_id,': item_ids, ecommerce_id: this.rest.ecommerce.id });
+
+			return Promise.all([ item_info_list, this.rest.getEcommerceItems(ecommerce_item_params) ]);
+		})
+		.then(([ cart_items, ecommerce_items_response ]) =>
+		{
+			cart_items.forEach((cart_item:any) => {
+				const ecommerce_item = ecommerce_items_response.data.find((ei:any) => ei.item_id === cart_item.item_id);
+				if (ecommerce_item) {
+					cart_item.ecommerce_item = ecommerce_item;
+				}
+			});
+			this.cart_items = cart_items;
+			this.updateTotal();
+		})
+	}
+
 	updateTotal()
 	{
 		this.total = this.cart_items.reduce((total, item) =>
 		{
+			console.log( item);
+			let price = item?.prices?.find((price:any)=>
+			{
+				return true;
+			});
+
 			const quantity = item.qty || 0;
-			const price = item.ecommerce_item?.price || 0;
-			return total + (quantity * price);
+			return total + (quantity * (price?.price || 0));
 		}, 0);
 
 		this.total_qty = this.cart_items.reduce((total, item) =>
--- src/app/pages/confirm-order/confirm-order.component.ts
+++ src/app/pages/confirm-order/confirm-order.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe, DecimalPipe } from '@angular/common';
 import { SearchUserComponent } from '../../components/search-user/search-user.component';
 
 @Component({
 	selector: 'app-confirm-order',
 	standalone: true,
-	imports: [HeaderComponent, FormsModule, CommonModule, SearchUserComponent],
+	imports: [HeaderComponent, FormsModule, CommonModule, SearchUserComponent, CurrencyPipe, DecimalPipe],
 	templateUrl: './confirm-order.component.html',
 	styleUrls: ['./confirm-order.component.css']
 })
@@ -25,12 +25,129 @@ export class ConfirmOrderComponent extends BaseComponent implements OnInit {
 	order_id: number | null = null;
 	selected_user_id: number | null = null;
 
+	public cart_items: any[] = [ ];
+	total: number = 0;
+	total_qty:number = 0;
+
 	ngOnInit() {
 		this.fetchAddresses();
 		if (!this.re_admin()) {
 			this.selected_user_id = this.rest.ecommerce_user.id;
 		}
+		this.fetchCartItems2();
 	}
+
+	public fetchCartItems()
+	{
+		let cart_items = this.rest.getCartItems();
+
+		if (cart_items.length === 0) {
+			this.cart_items = [];
+			return;
+		}
+
+		let item_ids = cart_items.map(item => item.item_id).join(',');
+
+		let params = this.rest.getUrlParams({ 'id,': item_ids })
+
+		this.rest.getItems(params)
+		.then((response) =>
+		{
+			if( response.data.length == 0 )
+			{
+				this.cart_items = [];
+				return;
+			}
+
+			this.cart_items = response.data.map((item_info:any) =>
+			{
+				let cart_item = cart_items.find((item) => item.item_id === item_info.item.id);
+				let image_url = item_info.item.image_id ? this.rest.base_url + '/image.php?id=' + item_info.item.image_id : null;
+				item_info.price = item_info.prices.find((price:any)=>true);
+				return {...cart_item, ...item_info, image_url};
+			});
+
+			this.updateTotal();
+
+			const ecommerce_item_params = this.rest.getUrlParams({ 'item_id,': item_ids, ecommerce_id: this.rest.ecommerce.id });
+			this.rest.getEcommerceItems(ecommerce_item_params).then((ecommerce_items_response) => {
+				this.cart_items.forEach((cart_item) => {
+					const ecommerce_item = ecommerce_items_response.data.find((ei:any) => ei.item_id === cart_item.item_id);
+					if (ecommerce_item) {
+						cart_item.ecommerce_item = ecommerce_item;
+					}
+				});
+				this.updateTotal();
+			});
+		})
+	}
+
+	public fetchCartItems2()
+	{
+		let cart_items = this.rest.getCartItems();
+
+		if (cart_items.length === 0) {
+			this.cart_items = [];
+			return;
+		}
+
+		let item_ids = cart_items.map(item => item.item_id).join(',');
+
+		let params = this.rest.getUrlParams({ 'id,': item_ids })
+
+		Promise.all([ cart_items, this.rest.getItems(params) ])
+		.then(([ cart_items, response ]) =>
+		{
+			console.log(cart_items);
+			let item_info_list:any[] = response.data.map((item_info:any) =>
+			{
+				console.log(item_info);
+				item_info.price = item_info.prices.find((price:any)=>true);
+				let cart_item = cart_items.find((item) => item.item_id === item_info.item.id);
+				console.log('c+item',cart_item, item_info);
+				let image_url = item_info.item.image_id ? this.rest.base_url + '/image.php?id=' + item_info.item.image_id : null;
+				return {...cart_item, ...item_info, image_url};
+			});
+			console.log(item_info_list);
+
+			const ecommerce_item_params = this.rest.getUrlParams({ 'item_id,': item_ids, ecommerce_id: this.rest.ecommerce.id });
+
+			return Promise.all([ item_info_list, this.rest.getEcommerceItems(ecommerce_item_params) ]);
+		})
+		.then(([ cart_items, ecommerce_items_response ]) =>
+		{
+			cart_items.forEach((cart_item:any) => {
+				const ecommerce_item = ecommerce_items_response.data.find((ei:any) => ei.item_id === cart_item.item_id);
+				if (ecommerce_item) {
+					cart_item.ecommerce_item = ecommerce_item;
+				}
+			});
+			this.cart_items = cart_items;
+			this.updateTotal();
+		})
+	}
+
+	updateTotal()
+	{
+		this.total = this.cart_items.reduce((total, item) =>
+		{
+			console.log( item);
+			let price = item?.prices?.find((price:any)=>
+			{
+				return true;
+			});
+
+			const quantity = item.qty || 0;
+			return total + (quantity * (price?.price || 0));
+		}, 0);
+
+		this.total_qty = this.cart_items.reduce((total, item) =>
+		{
+			const quantity = item.qty || 0;
+			return total + quantity;
+		}, 0);
+	}
+
 
 	onUserSelected(user: any) {
 		this.selected_user_id = user.id;
@@ -126,3 +243,4 @@ export class ConfirmOrderComponent extends BaseComponent implements OnInit {
 		}
 	}
 }
+
